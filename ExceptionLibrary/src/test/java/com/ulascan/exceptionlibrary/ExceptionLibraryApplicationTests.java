ecb5e54de4056262fd577df285ab4cd5c252f60c
S. Ulaşcan Kılıç <ulascank1@gmail.com>
2023-06-19 18:58:55 +0300

    initial commit

---
+++ ExceptionLibrary/src/test/java/com/ulascan/exceptionlibrary/ExceptionLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ulascan.exceptionlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
